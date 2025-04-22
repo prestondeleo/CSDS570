90095d565dbcc5260da6599284aa5fa39de69d81
Timothy Cronin <tcroniniv@gmail.com>
2025-04-22 15:02:07 -0400

    add dist calc between other embeddings

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from diffusion_model import cond_samp_CFG
 from BackwardPass import BackwardPass as Diffusion
 from vae import VAE
 from dirs import *
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, Subset
 
 def main(unknown: int):
     T = 1000
@@ -22,12 +24,30 @@ def main(unknown: int):
 
     vae.load_state_dict(torch.load(vae_path, weights_only=True, map_location=torch.device('cpu')))
     dif.load_state_dict(torch.load(dif_path, weights_only=True, map_location=torch.device('cpu')))
-    print(1)
-    samples = cond_samp_CFG(dif, (10, 1, 28, 28), class_token=2, constantw_scale=20)
-    print(2)
-    samples = (samples.clamp(-1, 1) + 1) / 2
-    torchvision.utils.save_image(samples, os.path.join(GENERATED_DIR, f'{unknown}_dif.png'),
-                                 nrow=len(samples) // 5)
+
+    k = 9
+    full_train = datasets.MNIST(root=DATASET_DIR, train=True, transform=transforms.Compose([
+        transforms.ToTensor()
+        ]), download=True)
+    indices = [i for i in range(len(full_train)) if full_train[i][1] != unknown]
+    train_data = Subset(full_train, indices)
+    train_loader = DataLoader(train_data, batch_size=128, shuffle=True)
+    vae.eval()
+    vae.save_distributions(train_loader)
+
+    num_samples = 20
+    unknown_class_instance = None
+    for d in full_train:
+        if d[1] == unknown:
+            unknown_class_instance = d
+            break
+    assert(unknown_class_instance is not None)
+    samp = unknown_class_instance[0].unsqueeze(0)
+    k_closest_classes = vae.k_closest(samp, k)
+    for c in k_closest_classes:
+        print(f'label: {c[0]} | dist: {c[1]}')
+
+
 
 if __name__ == '__main__':
     assert len(sys.argv) <= 2
